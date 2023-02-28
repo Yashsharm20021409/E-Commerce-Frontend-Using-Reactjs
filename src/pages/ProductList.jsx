c9aa25ef684cfef3cb3af48230e6ec281a866fb9
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2023-02-28 23:19:56 +0530

    update

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../Components/Announcement";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+import NewsLetter from "../Components/NewsLetter";
+import Product from "../Components/Product";
+
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 20px;
+`;
+const FilterComponent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* align-items: center; */
+`;
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
-  )
-}
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterComponent>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option selected disabled>
+              color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Green</Option>
+            <Option>Yellow</Option>
+            <Option>Blue</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterComponent>
+      <Product />
+      <NewsLetter />
+      <Footer />
+    </Container>
+  );
+};
 
-export default ProductList
+export default ProductList;
